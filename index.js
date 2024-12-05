9f94178a97f1a3c7b23c1080bf188340f95a5a79
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2024-12-05 19:36:15 +0500

    Add logic to play a single round

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
-const prompt = require('prompt-sync')();
-let getHumanChoice = prompt("your choice: ").toLowerCase();
-function getComputerChoice  () {
-   const randomNumber = Math.floor(Math.random() * 3);
-    if (randomNumber === 0) {
-    return("rock");
-    }
-    else if (randomNumber === 1) {
-    return("paper");
-    }
-    else {
-    return("scissors")
-    }
+const prompt = require("prompt-sync")();
+
+// Function to get human choice
+function getHumanChoice() {
+    return prompt("Your choice (rock, paper, scissors): ").toLowerCase();
 }
 
+// Logic to get human choice
+let humanChoice = getHumanChoice();
+
+// Start the loop to get a valid human choice
+do {
+    console.log(`You entered: ${humanChoice}`);
+} while (
+    humanChoice !== "rock" &&
+    humanChoice !== "paper" &&
+    humanChoice !== "scissors"
+);
+
+//make func to get comp choice
+function getComputerChoice () {
+    const randomNumber = Math.floor(Math.random() * 3);
+        if (randomNumber === 0) {
+        return("rock");
+        }
+        else if (randomNumber === 1) {
+        return("paper");
+        }
+        else {
+        return("scissors")
+        }    
+}
 const computerChoice = getComputerChoice();
 console.log("Computer choice: " + computerChoice);
 
-let humanScore = 0
 
-let computerScore = 0
+//vars to track scores
+var humanScore = 0
+var computerScore = 0
 
-function playRound(humanChoice, computerChoice) {
+
+//logic to play a single round
+function playRound (humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
         console.log("tie!")
     }
@@ -29,14 +49,11 @@ function playRound(humanChoice, computerChoice) {
         (computerChoice === "paper" && humanChoice === "rock") ||
         (computerChoice === "scissors" && humanChoice === "paper")
     ) {
-        console.log("you loose!")
+        console.log("you lose!")
     }
     else {
         console.log("you win!")
     }
 }
-  
-const humanSelection = getHumanChoice;
-const computerSelection = computerChoice;
- 
-playRound(humanSelection, computerSelection);
+
+playRound(humanChoice, computerChoice)
